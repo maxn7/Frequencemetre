1db7d339f569483f56412a54da94331a45fcb454
Maxime Morin <maxime.morin7@gmail.com>
2012-05-06 17:44:35 +0200

    Les fichiers sources suffiront...

---
+++ test_trouver_digit.vhd
@@ -0,0 +1,117 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   13:15:17 05/06/2012
+-- Design Name:   
+-- Module Name:   /home/max/ProjetNumerique/frequencemetre/test_trouver_digit.vhd
+-- Project Name:  frequencemetre
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: trouver_digit
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY test_trouver_digit IS
+END test_trouver_digit;
+ 
+ARCHITECTURE behavior OF test_trouver_digit IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT trouver_digit
+    PORT(
+         frequence : IN  std_logic_vector(23 downto 0);
+         puissance : IN  std_logic_vector(2 downto 0);
+         position_point : IN  std_logic_vector(1 downto 0);
+         digit : OUT  std_logic_vector(3 downto 0);
+         point_on : OUT  std_logic;
+         clk : IN  std_logic;
+         rst : IN  std_logic;
+         selecteur : OUT  std_logic_vector(3 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal frequence : std_logic_vector(23 downto 0) := (others => '0');
+   signal puissance : std_logic_vector(2 downto 0) := (others => '0');
+   signal position_point : std_logic_vector(1 downto 0) := (others => '0');
+   signal clk : std_logic := '0';
+   signal rst : std_logic := '0';
+
+ 	--Outputs
+   signal digit : std_logic_vector(3 downto 0);
+   signal point_on : std_logic;
+   signal selecteur : std_logic_vector(3 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 20 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: trouver_digit PORT MAP (
+          frequence => frequence,
+          puissance => puissance,
+          position_point => position_point,
+          digit => digit,
+          point_on => point_on,
+          clk => clk,
+          rst => rst,
+          selecteur => selecteur
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Init process
+   init_proc: process
+   begin		
+      rst <= '0';-- hold reset state for 100 ns.
+      wait for 50 ns;	
+      rst <= '1';
+      wait;
+   end process;
+	
+	 -- stimulus process
+   stim_proc: process
+   begin	
+	puissance <= "010";
+	frequence <= X"0000EF";
+	
+	wait; 
+   end process;
+	
+	
+
+END;
---
+++ trouver_digit.vhd
@@ -0,0 +1,166 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:30:12 04/23/2012 
+-- Design Name: 
+-- Module Name:    trouver_digit - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity trouver_digit is
+    Port ( frequence : in  STD_LOGIC_VECTOR (23 downto 0);
+           puissance : in  STD_LOGIC_VECTOR (2 downto 0);
+           position_point : in  STD_LOGIC_VECTOR (1 downto 0);
+           digit : out  STD_LOGIC_VECTOR (3 downto 0);
+			  point_on : out STD_LOGIC;
+			  clk : in STD_LOGIC;
+			  rst : in STD_LOGIC;
+           selecteur : out  STD_LOGIC_VECTOR (3 downto 0));
+end trouver_digit;
+
+architecture Behavioral of trouver_digit is
+
+type etat is (init,calc_digit,attente, incremente,affiche,calc_comp,soustraction);
+signal etatf : etat; --etat futur
+signal etatp : etat; --etat present
+
+signal Spuissance : STD_LOGIC_VECTOR (2 downto 0); 
+signal Sfrequence : STD_LOGIC_VECTOR (23 downto 0);
+signal comp : STD_LOGIC_VECTOR (23 downto 0);
+signal Sdigit : STD_LOGIC_VECTOR (3 downto 0);
+signal step : STD_LOGIC_VECTOR (1 downto 0);
+
+
+begin
+
+--Bloc F
+process(etatp, rst)
+begin
+	if rst='0' then etatf <=init ;
+	else	
+		case etatp is 
+		when init => etatf <= calc_comp;
+		
+		when calc_comp => etatf <= soustraction;
+
+		when soustraction => etatf <= calc_digit;
+		
+		when calc_digit => 
+			if(Sfrequence < comp) then etatf <= affiche;
+			else etatf <= soustraction;
+			end if;
+
+		when affiche => etatf<= attente; 
+		
+		when attente =>
+			if(comp = X"00C350") then
+				if(step = "00") then etatf <= init;
+				else etatf <= calc_comp;
+				end if;
+			else etatf <= incremente;
+			end if;
+			
+		when incremente => etatf <= attente;
+			
+		when others => etatf <= init;
+		
+		end case;
+	end if;
+end process;
+
+--Bloc M
+process(clk, rst)
+begin
+	if (clk'event and clk = '1') then etatp <= etatf ;
+	end if;
+end process;
+
+--Bloc G
+process(clk)
+begin
+if(clk'event and clk='1') then
+	
+	case etatp is
+	when init => step <= "00"; Sfrequence <= frequence; Spuissance<=puissance;
+	
+	when calc_comp => Sdigit <= "0001";
+				if(Spuissance="110") then comp <= X"0F4240"; --10^6
+				elsif(Spuissance="101") then comp <= X"0186A0"; --10^5
+				elsif(Spuissance="100") then comp <= X"002710"; --10^4
+				elsif(Spuissance="011") then comp <= X"0003E8"; --10^3
+				elsif(Spuissance="010") then comp <= X"000064"; --10^2
+				elsif(Spuissance="001") then comp <= X"00000A"; --10^1
+				elsif(Spuissance="000") then comp <= X"000001"; --10^0
+				else comp <= X"000001"; --10^0
+				end if;
+			
+	when soustraction => Sfrequence <= Sfrequence - comp;
+	
+	when calc_digit => 
+			if(Sfrequence >= comp) then Sdigit <= Sdigit + 1;
+			--elsif(Sfrequence = comp)  then Sdigit <= Sdigit + 1;
+			end if;
+			
+	when affiche => 
+			step <= step - 1 ; 
+			Spuissance <= Spuissance - 1; 
+			comp <= X"000000"; --Le signal va servir pour le comptage
+			digit <= Sdigit;
+			--dec_sel affiche valeur
+			--dec_point affiche point	
+			
+	when incremente =>
+			comp <= comp + 1; -- c'est le bloc F qui vérifie si on dépasse la valeur
+	
+	when others => 
+			
+	end case;
+
+end if;	
+end process;
+
+--dec_sel
+process(step)
+begin
+	if(step="11") then selecteur <= "1000";
+	elsif(step="10") then selecteur <= "0100";
+	elsif(step="01") then selecteur <= "0010";
+	else selecteur <= "0001";
+	end if;
+end process;
+
+--point_sel
+process(position_point, step)
+begin
+	if(position_point = step) then point_on <='1';
+	else point_on <= '0';
+	end if;
+end process;
+
+		
+end Behavioral;
+
